2c5d7941dc8e9a38eb74779f37905b1a204b5294
shaikrehaan <shabbirshaik1011@gmail.com>
2023-07-14 15:08:37 +0530

    msg2

---
+++ method1.java
@@ -0,0 +1,23 @@
+package samplejava;
+
+public class method1 {
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+		//program p1=new program();
+		//p1.addNumbers();
+double d=10.23;
+//int c=d;
+System.out.println(d);
+	}
+	char addNumbers( )
+	{
+	      // int c=a+b;
+		// char c=a;
+		 //long c=a+b;
+		 //System.out.println(c);
+		 return 'p';
+	}
+	 
+	
+}
